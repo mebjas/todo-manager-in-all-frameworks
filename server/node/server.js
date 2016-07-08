c3d2aaf145c908df8a9b8b71c333f2db2fb34582
Minhaz A V <minhazav@gmail.com>
2016-07-09 02:52:54 +0530

    react todo manager added

--- server/node/server.js
+++ server/node/server.js
@@ -19,17 +19,25 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
+// TODO: Correct the usage of this middleware
 // a middleware function with no mount path. This code is executed for every request to the router
 router.use(function (req, res, next) {
+	console.log('request recieved, ', req);
 	res.set('Access-Control-Allow-Origin', '*');
+	res.set('Access-Control-Allow-Methods','GET,POST');
+    res.set('Access-Control-Allow-Headers','X-Requested-With,Content-Type');
 	next();
 });
 
 
 // Read all todos
 app.get('/select', function(req, res){
+	res.set('Access-Control-Allow-Origin', '*');
 	try {
 		var data = db.getData("/");
+		data.todo.forEach(function(obj, index) {
+			this.todo[index]['ID'] = index;
+		}.bind(data));
 	    res.send(JSON.stringify(data, null, 3));
 	} catch (ex) {
 		res.send(JSON.stringify({error: true, exception: ex.message}));
@@ -37,7 +45,7 @@ app.get('/select', function(req, res){
 });
 
 // Uodate a todo
-app.post('/update', function(req, res){
+app.get('/update', function(req, res){
 	var title = '';
 	var ID = 0;
 	try {
@@ -60,6 +68,7 @@ app.post('/update', function(req, res){
 
 // Create a todo
 app.post('/create', function(req, res){
+	res.set('Access-Control-Allow-Origin', '*');
 	var title = '';
 	try {
 		title = req.body.title;
@@ -68,18 +77,25 @@ app.post('/create', function(req, res){
 		return;
 	}
 
-	var at = new Date().getTime();
-	db.push('/todo[]', {title: title, at: at});
 
 	try {
-    	res.send(JSON.stringify({error: false, data: db.getData('/todo')}, null, 3));
+		db.push('/todo[]', {title: title, at: new Date().getTime()});
+		var data = db.getData('/todo');
+
+		var c = 0;
+		data.forEach(function(d, index) {
+			data[index]['ID'] = ++c;
+		});
+    	res.send(JSON.stringify({error: false, data: data}, null, 3));
 	} catch (ex) {
+		console.log(ex);
 		res.send(JSON.stringify({error: true, exception: ex.message}));
 	}
 });
 
 // Delete a todo
 app.post('/delete', function(req, res){
+	res.set('Access-Control-Allow-Origin', '*');
 	var ID;
 	try {
 		ID = req.body.ID;
@@ -96,7 +112,13 @@ app.post('/delete', function(req, res){
 	}	
 
 	try {
-    	res.send(JSON.stringify({error: false, data: db.getData('/todo')}, null, 3));
+		var data = db.getData('/todo');
+
+		var c = 0;
+		data.forEach(function(d, index) {
+			data[index]['ID'] = ++c;
+		});
+    	res.send(JSON.stringify({error: false, data: data}, null, 3));
 	} catch (ex) {
 		res.send(JSON.stringify({error: true, exception: ex.message}));
 	}
